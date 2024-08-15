a8c85b9cc92aa9885ca4c7e3a520d0536c6a580d
DanielLumbu <daniellumbus@gmail.com>
2024-08-15 12:31:16 -0500

    Initial Commit

---
+++ Remember To Remember/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remember To Remember
+//
+//  Created by Daniel Lumbu on 8/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
